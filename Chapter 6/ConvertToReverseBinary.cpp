64aa91e30f23eae703d4e16e8e77aeaf55cfe63e
DanNano <93101866+DanNano@users.noreply.github.com>
2022-07-21 03:22:40 -0400

    Create ConvertToReverseBinary.cpp
    Write a program that takes in a positive integer as input, and outputs a string of 1's and 0's representing the integer in reverse binary.

---
+++ Chapter 6/ConvertToReverseBinary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int x;
+
+	cin >> x;
+
+	while (x > 0)
+	{
+		cout << x % 2;
+		x = x / 2;
+	}
+	cout << endl;
+	return 0;
+}
