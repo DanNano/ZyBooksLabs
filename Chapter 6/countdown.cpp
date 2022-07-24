fc0764dbb35c99c16035cf05d15572dca172f96b
DanNano <93101866+DanNano@users.noreply.github.com>
2022-07-24 02:57:44 -0400

    Create countdown.cpp

---
+++ Chapter 6/countdown.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+	int input;
+	cin >> input;
+
+	if (input >= 11 && input <= 100)
+	{
+		cout << input << " ";
+
+		while ((input % 11) != 0)
+		{
+			input--;
+			cout << input << " ";
+		}
+		cout << endl;
+	}
+	else
+	{
+		cout << "Input must be 11-100" << endl;
+	}
+
+	return 0;
+}
